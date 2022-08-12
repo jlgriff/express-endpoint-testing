a1e827edfb1f1c801cb4c7c603e5a8b4fe2c0ed2
John Griffith <john@John-MacBook-Air.local>
2022-08-12 16:27:19 -0400

    Fixed both endpoint tests for each of the 2 resolvers and added response validation.

--- src/graphql/test/resolvers.test.ts
+++ src/graphql/test/resolvers.test.ts
@@ -1,91 +1,84 @@
+/* eslint-disable object-curly-newline */
 import { Mongoose } from 'mongoose';
 import { StartedTestContainer } from 'testcontainers';
 import agent from 'supertest';
-import { getHashedPassword, startMongoClient } from './utilities';
+import { loginQueryString, startMongoClient, insertTestUser, responseHasUserIdAndToken, createUserMutationString, responsesMatch } from './utilities';
 import application from '../../app';
 import { ApplicationConfig } from '../../interfaces/config.app';
 import { graphqlEndpoint } from '../../configs';
-import UserModel from '../../models/user';
+import { validUserInput } from './test-data';
+import log from '../../utilities/logger';
 
 const appConfig: ApplicationConfig = { graphiqlEnabled: false };
 const app = application(appConfig);
 const dbPort = 27017;
 
-const validUserInput = {
-  email: 'test@test.com',
-  password: '123456',
-  firstname: 'John',
-  lastname: 'Doe',
-};
+let container: StartedTestContainer;
+let client: Mongoose;
 
-const loginQueryString = (email: string, password: string): string => `
-  query {
-    login(email: "${email}", password: "${password}") {
-      userId token
-    }
-  }
-`;
+beforeAll(async () => {
+  const { testContainer, testClient } = await startMongoClient(dbPort);
+  container = testContainer;
+  client = testClient;
+  return client.connection.db.dropDatabase();
+});
 
-const createUserMutationString = (email: string, firstname: string, lastname: string, password: string): string => ` 
-  mutation { 
-    createUser(email: "${email}", firstname: "${firstname}", lastname: "${lastname}", password: "${password}") { 
-      _id email firstname lastname 
-    } 
-  }
-`;
+afterAll(async () => {
+  await client.connection.close();
+  return container.stop();
+});
 
-describe('Test each resolver', () => {
-  let container: StartedTestContainer;
-  let client: Mongoose;
+afterEach(async () => client.connection.db.dropDatabase());
 
-  beforeAll(async () => {
-    const { testContainer, testClient } = await startMongoClient(dbPort);
-    container = testContainer;
-    client = testClient;
-  });
+describe('Test the login resolver', () => {
+  it('login: Verify that a user with correct credentials can log in', async () => {
+    const { firstname, lastname, password } = validUserInput;
+    const email = 'valid.user1@test.com';
+    await insertTestUser(email, firstname, lastname, password);
 
-  beforeEach(async () => {
-    await client.connection.db.dropDatabase();
-  });
-
-  afterAll(async () => {
-    await client.connection.close();
-    await container.stop();
-  });
-
-  it('Verify that a user with correct credentials can log in', async () => {
-    const {
-      email, firstname, lastname, password,
-    } = validUserInput;
-
-    const userModel = new UserModel({
-      email,
-      firstname,
-      lastname,
-      password: await getHashedPassword(password),
-    });
-    await userModel.save();
-
-    const response: agent.Test = agent(app)
+    await agent(app)
       .post(graphqlEndpoint)
+      .set('Accept', 'application/json')
       .send({ query: loginQueryString(email, password) })
-      .set('Accept', 'application/json');
-
-    response
-      .expect(200);
+      .expect(200)
+      .expect((res) => {
+        const { body } = res;
+        if (!responseHasUserIdAndToken(body)) {
+          const error: Error = new Error(`Response does not contain userId and token:\n${JSON.stringify(body)}`);
+          log('error', error.toString());
+          throw error;
+        }
+      });
   });
+});
 
-  it('Verify that a new valid user can be created', async () => {
-    const {
-      email, firstname, lastname, password,
-    } = validUserInput;
+describe('Test the createUser resolver', () => {
+  it('createUser: Verify that a new valid user can be created', async () => {
+    const { firstname, lastname, password } = validUserInput;
+    const email = 'valid.user1@test.com';
+    const expected = {
+      data: {
+        createUser: {
+          _id: '                        ',
+          email,
+          firstname,
+          lastname,
+        },
+      },
+    };
 
-    const response: agent.Test = agent(app)
+    await agent(app)
       .post(graphqlEndpoint)
+      .set('Accept', 'application/json')
       .send({ query: createUserMutationString(email, firstname, lastname, password) })
-      .set('Accept', 'application/json');
-
-    response
-      .expect(200);
+      .expect(200)
+      .expect((res) => {
+        const { body } = res;
+        if (!responsesMatch(expected, body)) {
+          const error: Error = new Error(`Response not match expected output:\n Expected:\n ${JSON.stringify(expected)}\n Actual:\n ${JSON.stringify(body)}`);
+          log('error', error.toString());
+          throw error;
+        }
+      });
   });
 });
--- src/graphql/test/utilities.ts
+++ src/graphql/test/utilities.ts
@@ -4,7 +4,11 @@ import { RedisClientType, createClient } from 'redis';
 import { GenericContainer, StartedTestContainer } from 'testcontainers';
 import isEqual from 'lodash.isequal';
 import sortBy from 'lodash.sortby';
+import supertest from 'supertest';
+import { User } from '../../interfaces/user';
 import UserModel from '../../models/user';
+import log from '../../utilities/logger';
+import { Exception } from '../../interfaces/exception';
 
 /**
  * Starts up a temporary redis container and uses it to initialize a redis client
@@ -49,6 +53,18 @@ export const startMongoClient = async (port: number): Promise<{
 };
 
 /**
+ * Logs the supertest error and then throws it
+ */
+export const handleTestError = (err: any, res: supertest.Response) => {
+  if (err) {
+    const errorMsg: string = `Test's error response:\n${JSON.stringify(res)}`;
+    log('error', errorMsg);
+    const error: Exception = { message: errorMsg, status: 500 };
+    throw error;
+  }
+};
+
+/**
  * Hashes the given password for pre-loading a hashed password into the database
  */
 export const getHashedPassword = async (
@@ -56,21 +72,43 @@ export const getHashedPassword = async (
 ): Promise<string> => bcrypt.hash(password, 12);
 
 /**
- * Creates and saves a user in the database with the given properties
+ * Queries the database for a particular user email
  */
-export const saveTestUser = async (
+export const findTestUser = async (email: string): Promise<User | null> => {
+  const existingUser = await UserModel.findOne({ email });
+  if (!existingUser) {
+    return null;
+  }
+  return {
+    _id: existingUser.id,
+    email: existingUser.email,
+    firstname: existingUser.firstname,
+    lastname: existingUser.lastname,
+  };
+};
+
+/**
+ * Inserts a test user into the User collection with the given properties
+ */
+export const insertTestUser = async (
   email: string,
   firstname: string,
   lastname: string,
   password: string,
-): Promise<void> => {
+): Promise<User> => {
   const userModel = new UserModel({
     email,
     firstname,
     lastname,
     password: await getHashedPassword(password),
   });
-  await userModel.save();
+  const createdUser = await userModel.save();
+  return {
+    _id: createdUser.id,
+    email,
+    firstname,
+    lastname,
+  };
 };
 
 /**
@@ -90,28 +128,20 @@ export const responsesMatch = (
     return (expected.errors.length === actual.errors.length
       && isEqual(sortBy(expected.errors), sortBy(actual.errors)));
   }
-  // Checks if both are matching login responses
-  if (
-    expected.data
-    && expected.data.login
-    && expected.data.login.userId
-    && actual.data
-    && actual.data.login
-    && actual.data.login.userId
-  ) {
-    return expected.data.login.userId === actual.data.login.userId;
-  }
   // Checks if both are matching createUser responses
   if (
     expected.data
     && expected.data.createUser
+    && expected.data.createUser.email
     && expected.data.createUser.firstname
     && expected.data.createUser.lastname
     && actual.data.createUser
+    && expected.data.createUser.email
     && actual.data.createUser.firstname
     && actual.data.createUser.lastname
   ) {
-    return expected.data.createUser.firstname === actual.data.createUser.firstname
+    return expected.data.createUser.email === actual.data.createUser.email
+      && expected.data.createUser.firstname === actual.data.createUser.firstname
       && expected.data.createUser.lastname === actual.data.createUser.lastname;
   }
   return false;
